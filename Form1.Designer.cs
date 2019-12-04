3bdd37bed7606d50069bce206fa12f356117378c
Rex2701 <58503026+Rex2701@users.noreply.github.com>
2019-12-04 21:18:42 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace Oscillator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartStop = new System.Windows.Forms.Button();
+            this.BackgroundThread = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // StartStop
+            // 
+            this.StartStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StartStop.Location = new System.Drawing.Point(118, 325);
+            this.StartStop.Name = "StartStop";
+            this.StartStop.Size = new System.Drawing.Size(88, 31);
+            this.StartStop.TabIndex = 1;
+            this.StartStop.Text = "Пуск";
+            this.StartStop.UseVisualStyleBackColor = true;
+            this.StartStop.Click += new System.EventHandler(this.StartStop_Click);
+            // 
+            // BackgroundThread
+            // 
+            this.BackgroundThread.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundThread_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 368);
+            this.Controls.Add(this.StartStop);
+            this.Name = "Form1";
+            this.Text = "Осциллятор";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartStop;
+        private System.ComponentModel.BackgroundWorker BackgroundThread;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Oscillator
+{
+    public partial class Form1 : Form
+    {
+        public bool State = false;
+        public float a = 0, b = 0;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            BackgroundThread.RunWorkerAsync();
+        }
+
+        private void StartStop_Click(object sender, EventArgs e)
+        {
+            State = !State;
+            if (State) StartStop.Text = "Стоп";
+            else StartStop.Text = "Пуск";
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            SolidBrush brush = new SolidBrush(Color.Black);
+            Pen pen = new Pen(brush);
+            g.DrawLine(pen, 160, 10, 160, 310);
+            g.DrawLine(pen, 10, 160, 310, 160);
+        }
+
+        private void BackgroundThread_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            while (State)
+            {
+                Graphics g = e.Graphics;
+                SolidBrush brush = new SolidBrush(Color.Black);
+                Pen pen = new Pen(brush);
+                g.DrawEllipse(pen, (float)(160 + 150 * Math.Cos(a)), (float)(160 - 150 * Math.Sin(b)), 5, 5);
+                a += 0.01F; b += 0.02F;
+            }
+        }
+    }
+}
